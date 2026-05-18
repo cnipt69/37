eabfe6923cd2e4f7f9ea643059f03f3fb58674aa
Your Name <cnipt69@gmail.com>
2026-05-18 15:32:23 +0500

    second commit

--- js/script.js
+++ js/script.js
@@ -1,45 +1,42 @@
 const tabs = document.querySelectorAll('.tabcontent');
 const items = document.querySelectorAll('.tabheader__item');
 
-    if (tabs[3]) {
-        const imgInFourthTab = tabs[3].querySelector('img');
-        if (imgInFourthTab) {
-            imgInFourthTab.src = 'img/tabs/hamburger.jpg';
-            imgInFourthTab.alt = 'hamburger';
-        }
+if (tabs[3]) {
+    const img = tabs[3].querySelector('img');
+    if (img) {
+        img.src = 'img/tabs/hamburger.jpg';
+        img.alt = 'hamburger';
     }
+}
+function resetTabs() {
+    tabs.forEach(tab => tab.style.display = 'none');
+    items.forEach(item => item.classList.remove('tabheader__item_active'));
+}
 
-    function resetTabs() {
-        tabs.forEach(tab => {
-            tab.style.display = 'none';
-            tab.classList.remove('active');
-        });
-        items.forEach(item => {
-            item.classList.remove('tabheader__item_active');
-        });
-    }
+resetTabs();
+tabs[0].style.display = 'block';
+items[0].classList.add('tabheader__item_active');
 
-    function activateTab(index) {
-        resetTabs();
-        tabs[index].style.display = 'block';
-        tabs[index].classList.add('active');
-        items[index].classList.add('tabheader__item_active');
-    }
+items[0].onclick = function() {
+    resetTabs();
+    tabs[0].style.display = 'block';
+    items[0].classList.add('tabheader__item_active');
+};
 
-    activateTab(0);
+items[1].onclick = function() {
+    resetTabs();
+    tabs[1].style.display = 'block';
+    items[1].classList.add('tabheader__item_active');
+};
 
-    items[0].addEventListener('click', () => {
-        activateTab(0);
-    });
+items[2].onclick = function() {
+    resetTabs();
+    tabs[2].style.display = 'block';
+    items[2].classList.add('tabheader__item_active');
+};
 
-    items[1].addEventListener('click', () => {
-        activateTab(1);
-    });
-
-    items[2].addEventListener('click', () => {
-        activateTab(2);
-    });
-
-    items[3].addEventListener('click', () => {
-        activateTab(3);
-    });
+items[3].onclick = function() {
+    resetTabs();
+    tabs[3].style.display = 'block';
+    items[3].classList.add('tabheader__item_active');
+};
