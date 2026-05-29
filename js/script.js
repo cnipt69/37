f507058ec2540c2c1d851dd6b5d54166421c3f8c
Your Name <cnipt69@gmail.com>
2026-05-29 14:56:52 +0500

    newFormData

--- js/script.js
+++ js/script.js
@@ -1,69 +1,118 @@
-const genderButtons = document.querySelectorAll('#gender .calculating__choose-item');
-const activityButtons = document.querySelectorAll('.calculating__choose_big .calculating__choose-item');
+// const genderButtons = document.querySelectorAll('#gender .calculating__choose-item');
+// const activityButtons = document.querySelectorAll('.calculating__choose_big .calculating__choose-item');
 
-const heightInput = document.getElementById('height');
-const weightInput = document.getElementById('weight');
-const ageInput = document.getElementById('age');
-const result = document.querySelector('.calculating__result span');
+// const heightInput = document.getElementById('height');
+// const weightInput = document.getElementById('weight');
+// const ageInput = document.getElementById('age');
+// const result = document.querySelector('.calculating__result span');
 
-let selectedGender = 'female';
-let selectedActivity = 1.375;
+// let selectedGender = 'female';
+// let selectedActivity = 1.375;
 
 
-genderButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        genderButtons.forEach(btn => btn.classList.remove('calculating__choose-item_active'));
-        button.classList.add('calculating__choose-item_active');
+// genderButtons.forEach(button => {
+//     button.addEventListener('click', () => {
+//         genderButtons.forEach(btn => btn.classList.remove('calculating__choose-item_active'));
+//         button.classList.add('calculating__choose-item_active');
 
-        if (button.textContent === 'Женщина') {
-            selectedGender = 'female';
-        } else {
-            selectedGender = 'male';
-        }
+//         if (button.textContent === 'Женщина') {
+//             selectedGender = 'female';
+//         } else {
+//             selectedGender = 'male';
+//         }
 
-        calculateCalories();
-    });
+//         calculateCalories();
+//     });
+// });
+
+
+// activityButtons.forEach(button => {
+//     button.addEventListener('click', () => {
+
+//         activityButtons.forEach(btn => btn.classList.remove('calculating__choose-item_active'));
+//         button.classList.add('calculating__choose-item_active');
+
+//         selectedActivity = Number(button.dataset.activity);
+
+//         calculateCalories();
+//     });
+// });
+
+
+// function calculateCalories() {
+//     const height = Number(heightInput.value);
+//     const weight = Number(weightInput.value);
+//     const age = Number(ageInput.value);
+
+//     if (!height || !weight || !age) {
+//         result.textContent = '0';
+//         return;
+//     }
+
+//     let calories;
+
+//     if (selectedGender === 'female') {
+//         calories = 655.1 + (9.563 * weight) + (1.85 * height) - (4.676 * age);
+//     } else {
+//         calories = 66.5 + (13.75 * weight) + (5.003 * height) - (6.775 * age);
+//     }
+
+//     calories = calories * selectedActivity;
+
+//     result.textContent = Math.round(calories);
+// }
+
+// calculateCalories();
+
+// heightInput.addEventListener('input', calculateCalories);
+// weightInput.addEventListener('input', calculateCalories);
+// ageInput.addEventListener('input', calculateCalories);
+
+
+const modal = document.querySelector('.modal');
+const openButtons = document.querySelectorAll('[data-modal]')
+const closeButton = document.querySelector('[data-close]');
+
+openButtons.forEach(button => {
+    button.onclick = () => {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+    };
 });
 
+closeButton.onclick = () => {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+};
 
-activityButtons.forEach(button => {
-    button.addEventListener('click', () => {
+const orderForm = document.querySelector('.order__form');
+const modalForm = document.querySelector('.modal form');
 
-        activityButtons.forEach(btn => btn.classList.remove('calculating__choose-item_active'));
-        button.classList.add('calculating__choose-item_active');
-
-        selectedActivity = Number(button.dataset.activity);
-
-        calculateCalories();
-    });
-});
+let freeDayData = {};
+let callbackData = {};
 
 
-function calculateCalories() {
-    const height = Number(heightInput.value);
-    const weight = Number(weightInput.value);
-    const age = Number(ageInput.value);
+orderForm.onsubmit = (event) => {
+    event.preventDefault();
 
-    if (!height || !weight || !age) {
-        result.textContent = '0';
-        return;
+    let data = new FormData(orderForm);
+
+    for (const [key, value] of data) {
+        freeDayData[key] = value;
     }
 
-    let calories;
+    console.log(freeDayData);
+};
 
-    if (selectedGender === 'female') {
-        calories = 655.1 + (9.563 * weight) + (1.85 * height) - (4.676 * age);
-    } else {
-        calories = 66.5 + (13.75 * weight) + (5.003 * height) - (6.775 * age);
+
+modalForm.onsubmit = (event) => {
+    event.preventDefault();
+
+    let data = new FormData(modalForm);
+
+    for (const [key, value] of data) {
+        callbackData[key] = value;
     }
 
-    calories = calories * selectedActivity;
-
-    result.textContent = Math.round(calories);
-}
-
-calculateCalories();
-
-heightInput.addEventListener('input', calculateCalories);
-weightInput.addEventListener('input', calculateCalories);
-ageInput.addEventListener('input', calculateCalories);
+    console.log(callbackData);
+};
